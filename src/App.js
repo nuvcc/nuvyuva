bee51705718c781c72ef027f46a3716b4fcaea61
shreyanshi <shahshreyanshi01@gamil.com>
2022-03-02 20:28:07 +0530

    added logo and countdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import "./App.css";
+import React,{useEffect, useRef, useState} from 'react';
 
-function App() {
+import "./App.css";
+import logo from './logo.png';
+
+const App = () =>{
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () =>{
+    const countdownDate = new Date('April 06, 2022 00:00:00').getTime()
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance/(1000*60*60*24));
+      const hours = Math.floor((distance%(1000*60*60*24)/(1000*60*60)));
+      const minutes = Math.floor((distance%(1000*60*60*24))/(1000*60));
+      const seconds = Math.floor((distance%(1000*60))/1000);
+
+      if(distance<0){
+        clearInterval(interval.current);
+      }else{
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() =>{
+    startTimer();
+    return() =>{
+      clearInterval(interval.current)
+    };
+  })
+
   return (
-    <div className="App" id="#App">
-      <div className="container"></div>
-      
-    </div>
+    <section className="App" id="#App">
+      <section className="container">
+      <section className="timer">
+      <div>
+          <img src={logo} className="logo" alt="logo"></img>
+      </div>
+      <div className="clock">
+        <section>
+          <p>{timerDays}</p>
+          <p><small>DAYS</small></p>
+        </section>
+        <span>:</span>
+        <section>
+          <p>{timerHours}</p>
+          <p><small>HOURS</small></p>
+        </section>
+        <span>:</span>
+        <section>
+          <p>{timerMinutes}</p>
+          <p><small>MINUTES</small></p>
+        </section>
+        <span>:</span>
+        <section>
+          <p>{timerSeconds}</p>
+          <p><small>SECONDS</small></p>
+        </section>
+      </div>
+      </section>
+      </section>
+    </section>
   );
 }
 
