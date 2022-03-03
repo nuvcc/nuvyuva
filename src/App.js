c0de31b6a1f84b7beca4b4ab07b5630aad2f7973
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2022-03-03 08:13:57 +0530

    Corrected minutes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () =>{
 
       const days = Math.floor(distance/(1000*60*60*24));
       const hours = Math.floor((distance%(1000*60*60*24)/(1000*60*60)));
-      const minutes = Math.floor((distance%(1000*60*60*24))/(1000*60));
+      const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
       const seconds = Math.floor((distance%(1000*60))/1000);
 
       if(distance<0){
