e600476e619d565dfd5787d0cd98e85f3c42d724
sheelahirrao16 <18103512@nuv.ac.in>
2022-03-06 20:01:32 +0530

    Integrated Countdown into Landing Page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Home from './components/pages';
 
 const App = () => {
   return (
-    <div>
       <Home />
-      <Router>
-        {/* <Home /> */}
-      </Router>
-    </div>
-    
-  );
+  )
 }
 
 export default App
