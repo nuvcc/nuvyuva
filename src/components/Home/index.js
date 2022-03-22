9714d000099d535ef0d6d1ce7455bfd35dd0ab30
shreyanshi <shahshreyanshi01@gamil.com>
2022-03-22 19:49:08 +0530

    added scrollToTop button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,6 +12,7 @@ import GoogleMaps from "./../Maps/GoogleMap";
 import Events from "../Events/Events";
 import About from "../About";
 import Nuv from "../Nuv";
+import ScrollToTop from "react-scroll-to-top";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,6 +33,7 @@ const Home = () => {
         <Members />
         <GoogleMaps />
       </BodyGradient>
+      <ScrollToTop smooth top="1000" color="#570658" viewbox="5 5 5 5" height="20" width="20"/>
       <Footer />
     </>
   );
