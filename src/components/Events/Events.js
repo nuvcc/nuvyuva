ff83dc5a203df19a1f2fec69ca8dacf0ea24ae06
Kalp Adhwaryu <kalpadhwaryu1581@gmail.com>
2022-03-23 21:42:59 +0530

    Changed Links of Events (#18)

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -6,35 +6,41 @@ import workshop from "../images/eventcategories/workshop.webp";
 import gaming from "../images/eventcategories/gaming.webp";
 import cultural from "../images/eventcategories/cultural.webp";
 import sports from "../images/eventcategories/sports.webp";
+import { Link } from "react-router-dom";
 const Circle1 = require("../images/circles/Circle1.png");
 
 const Events = () => {
   let slides = [
-    <a href="/technical">
+    <Link to="/technical">
       <img className="events-img" src={tech} alt="Technical Events" />
-    </a>,
-    <a href="/workshops">
+    </Link>,
+    <Link to="/workshops">
       <img className="events-img" src={workshop} alt="Workshops" />
-    </a>,
-    <a href="/gaming">
+    </Link>,
+    <Link to="/gaming">
       <img className="events-img" src={gaming} alt="Gaming Events" />
-    </a>,
-    <a href="/cultural">
+    </Link>,
+    <Link to="/cultural">
       <img className="events-img" src={cultural} alt="Cultural Events" />{" "}
-    </a>,
-    <a href="/sports">
+    </Link>,
+    <Link to="/sports">
       <img className="events-img" src={sports} alt="Sports Events" />{" "}
-    </a>,
+    </Link>,
   ];
 
   return (
     <div className="events">
-      <img src={Circle1} className="events-img1 circle-svgs" alt="circle-png"/>
-      <img src={Circle1} className="events-img2 circle-svgs" alt="circle-png"/>
-      <img src={Circle1} className="events-img3 circle-svgs" alt="circle-png"/>
+      <img src={Circle1} className="events-img1 circle-svgs" alt="circle-png" />
+      <img src={Circle1} className="events-img2 circle-svgs" alt="circle-png" />
+      <img src={Circle1} className="events-img3 circle-svgs" alt="circle-png" />
       <div className="event-heading">EVENTS</div>
       <div className="events-cards">
-        <Carousel slides={slides} autoplay={true} interval={2000} arrows={true} />
+        <Carousel
+          slides={slides}
+          autoplay={true}
+          interval={2000}
+          arrows={true}
+        />
       </div>
     </div>
   );
