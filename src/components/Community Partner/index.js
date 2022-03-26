03dbd16b9d895653c925cc90eb2a1878d2fd388d
shreyanshi <shahshreyanshi01@gamil.com>
2022-03-26 22:44:16 +0530

    added title sponsor & updated concert link

--- src/components/Community Partner/index.js
+++ src/components/Community Partner/index.js
@@ -5,7 +5,7 @@ import CDS from "./images/Group 100.svg";
 import GDC from "./images/Group 99.svg";
 import MozNUV from "./images/Group 101.svg";
 import Gameroo from "./images/Group 102.svg";
-import logo from './images/Kanan.png';
+import Kanan from './images/Kanan.svg';
 import { Container,row,col } from "react-bootstrap";
 import "./CommunityPartner.css";
 
@@ -33,6 +33,14 @@ export default function communityPartner() {
         </div>
         <div class="col-lg-2 col-md-4 col-sm-1">
           <img src={Hackclub} alt="Hackclub"/>
+        </div>
+      </div>
+      <div class="title2">
+        <h2>TITLE SPONSOR</h2>
+        <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+          <img src={Kanan} alt="kanan" className="kanan"/>
+        </div>
         </div>
       </div>
     </div>
