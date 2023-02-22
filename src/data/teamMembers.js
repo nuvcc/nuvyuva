b57a8a0785fd85846a346a391732856faaa9d403
nimit2801 <mnimitsavant@outlook.com>
2023-02-22 14:02:50 +0530

    make changes to ui and set for ci

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -1,161 +1,156 @@
 export const teamMembers = [
   {
-    category: "Presidents",
+    category: 'Presidents',
     members: [
       {
-        name: "Tej Gandhi",
-        quote: "President",
-        img: "images/team/TejGandhi.PNG",
+        name: 'Tej Gandhi',
+        quote: 'President',
+        img: 'images/team/TejGandhi.PNG',
       },
       {
-        name: "Zaahiya Kachwala",
-        quote: "Secretary",
-        img: "images/team/ZaahiyaKachwala.jpg",
+        name: 'Zaahiya Kachwala',
+        quote: 'Vice President',
+        img: 'images/team/ZaahiyaKachwala.jpg',
       },
     ],
   },
   {
-    category: "Secretaries",
+    category: 'Secretaries',
     members: [
       {
-        name: "Pratham Dalvi",
-        quote: "President",
-        img: "images/team/PrathamDalvi.jpeg"
+        name: 'Pratham Dalvi',
+        quote: 'Secretary',
+        img: 'images/team/PrathamDalvi.jpeg',
       },
       {
-        name: "Jashit Kandhari",
-        quote: "Secretary",
-        img: "images/team/JashitKandhari.jpeg",
+        name: 'Jashit Kandhari',
+        quote: 'Secretary',
+        img: 'images/team/JashitKandhari.jpeg',
       },
       {
-        name: "Rucha Soni",
-        quote: "Secretary",
-        img: "images/team/RuchaSoni.jpg",
+        name: 'Rucha Soni',
+        quote: 'Secretary',
+        img: 'images/team/RuchaSoni.jpg',
       },
     ],
   },
   {
-    category: "Treasurer",
+    category: 'Treasurer',
     members: [
       {
-        name: "Jaimin Mevada",
-        quote: "Treasurer",
-        img: "images/team/jaimin.webp"
+        name: 'Jaimin Mevada',
+        quote: 'Treasurer',
+        img: 'images/team/jaimin.webp',
       },
       {
-        name: "Ishaan Agrawal",
-        quote: "Co Treasurer",
-        img: "images/team/Ishaanagrawal.jpeg",
+        name: 'Ishaan Agrawal',
+        quote: 'Co Treasurer',
+        img: 'images/team/Ishaanagrawal.jpeg',
       },
     ],
   },
   {
-    category: "OUT REACH & SPONSER",
+    category: 'OUT REACH & SPONSER',
     members: [
       {
-        name: "Kush Rana",
-        quote: "OUT REACH & SPONSER",
-        img: "images/team/KUSHRANA.jpg",
+        name: 'Kush Rana',
+        quote: 'OUT REACH & SPONSER',
+        img: 'images/team/KUSHRANA.jpg',
       },
     ],
   },
   {
-    category: "Technical Team",
+    category: 'Technical Team',
     members: [
+      // {
+      //   name: 'Dhaval Fatani',
+      //   quote: 'Tech Head',
+      //   img: '',
+      // },
       {
-        name: "Dhaval Fatani",
-        quote: "Tech Head",
-        img: "",
+        name: 'Krish Savani',
+        quote: 'Tech Head',
+        img: 'images/team/KrishSavani.jpg',
       },
       {
-        name: "Krish Savani",
-        quote: "Tech Head",
-        img: "images/team/KrishSavani.jpg",
+        name: 'Pruthvi Patel',
+        quote: 'Web Developer',
+        img: 'images/team/PruthviPatel.jpg',
       },
       {
-        name: "Pruthvi Patel",
-        quote: "Web Developer",
-        img: "images/team/PruthviPatel.jpg",
-      },
-      {
-        name: "Jeet Shah",
-        quote: "Web Developer",
-        img: "images/team/JeetShah.jpg",
-      },
-      {
-        name: "Nimit Savant",
-        quote: "Technical Advisory",
-        img: "",
+        name: 'Jeet Shah',
+        quote: 'Web Developer',
+        img: 'images/team/JeetShah.jpg',
       },
     ],
   },
   {
-    category: "Decoration Team",
+    category: 'Decoration Team',
     members: [
       {
-        name: "Fatema Kapasi",
-        quote: "Head",
-        img: "images/team/FatemaKapasi.jpg",
+        name: 'Fatema Kapasi',
+        quote: 'Head',
+        img: 'images/team/FatemaKapasi.jpg',
       },
       {
-        name: "Hitakshi Agarwal",
-        quote: "Decoration",
-        img: "images/team/HitakshiAgarwal.jpeg"
+        name: 'Hitakshi Agarwal',
+        quote: 'Co Head',
+        img: 'images/team/HitakshiAgarwal.jpeg',
       },
       {
-        name: "Yesha Barot",
-        quote: "Co Head",
-        img: "images/team/YeshaBarot.jpg"
+        name: 'Yesha Barot',
+        quote: 'Co Head',
+        img: 'images/team/YeshaBarot.jpg',
       },
     ],
   },
   {
-    category: "DESIGN & PRINTING Team",
+    category: 'DESIGN & PRINTING Team',
     members: [
       {
-        name: "Sanjana Nathani",
-        quote: "Head",
-        img: "images/team/SanjanaNathani.jpeg",
+        name: 'Sanjana Nathani',
+        quote: 'Head',
+        img: 'images/team/SanjanaNathani.jpeg',
       },
       {
-        name: "Dev Soni",
-        quote: "Co-Head",
-        img: "images/team/DevSoni.jpg",
+        name: 'Dev Soni',
+        quote: 'Co-Head',
+        img: 'images/team/DevSoni.jpg',
       },
     ],
   },
 
+  // {
+  //   category: 'Social Media & Media Team',
+  //   members: [
+  //     {
+  //       name: 'Devansh Bhrambhatt',
+  //       quote: '',
+  //     },
+  //   ],
+  // },
   {
-    category: "Social Media & Media Team",
+    category: 'Cultural Team',
     members: [
       {
-        name: "Devansh Bhrambhatt",
-        quote: "",
+        name: 'Mansi Thakkar',
+        quote: 'Head',
+        img: 'images/team/MansiThakkar.jpeg',
       },
     ],
   },
   {
-    category: "Cultural Team",
+    category: 'Sports Team',
     members: [
+      // {
+      //   name: 'Naitik',
+      //   quote: 'Head',
+      //   img: '',
+      // },
       {
-        name: "Mansi Thakkar",
-        quote: "Head",
-        img: "images/team/MansiThakkar.jpeg"
-      },
-    ],
-  },
-  {
-    category: "Sports Team",
-    members: [
-      {
-        name: "Naitik",
-        quote: "Head",
-        img: ""
-      },
-      {
-        name: "Pratham Vajifdar",
-        quote: "Co-Head",
-        img: ""
+        name: 'Pratham Vajifdar',
+        quote: 'Co-Head',
+        img: 'images/team/Pratham Vajifdar.jpg',
       },
     ],
   },
@@ -186,55 +181,38 @@ export const teamMembers = [
   //   ],
   // },
   {
-    category: "ADIVSORY",
+    category: 'ADIVSORY',
     members: [
+      // {
+      //   name: 'Sachin',
+      //   quote: '',
+      //   img: '',
+      // },
+      // {
+      //   name: 'Shivam',
+      //   quote: '',
+      //   img: '',
+      // },
+      // {
+      //   name: 'Jaidev',
+      //   quote: '',
+      //   img: '',
+      // },
+      // {
+      //   name: 'Meet',
+      //   quote: '',
+      //   img: '',
+      // },
       {
-        name: "Sachin",
-        quote: "",
-        img: ""
+        name: 'Hardik Raval',
+        quote: '',
+        img: 'images/team/HARDIK.jpg',
       },
       {
-        name: "Shivam",
-        quote: "",
-        img: ""
-      },
-      {
-        name: "Jai Dev",
-        quote: "",
-        img: ""
-      },
-      {
-        name: "Meet",
-        quote: "",
-        img: ""
-      },
-      {
-        name: "Hardik",
-        quote: "",
-        img: "images/team/HARDIK.jpg"
-      },
-      {
-        name: "Nimit",
-        quote: "",
-        img: ""
+        name: 'Nimit Savant',
+        quote: '',
+        img: 'images/team/Nimit Savant.webp',
       },
     ],
   },
-  // {
-  //   category: "OVER ALL PLANING",
-  //   members: [
-  //     {
-  //       name: "Hardik",
-  //       quote: "",
-  //     },
-  //     {
-  //       name: "Tej Gandhi",
-  //       quote: "",
-  //     },
-  //     {
-  //       name: "Zaahiya",
-  //       quote: "",
-  //     },
-  //   ],
-  // },
 ];
