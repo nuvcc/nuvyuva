097cd700655fa7ad6e3be80bf315479ff7d97974
jeetshah47 <jeetshahajwa@gmail.com>
2023-02-22 16:22:42 +0530

    Images Src Fixed

--- src/components/Memories/MemoryCard.jsx
+++ src/components/Memories/MemoryCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { MCard, MCardHeading, MCardText } from "../../styles/Memories/Styles";
-import memoryImg from "../../images/Memory.png";
 import { CardDesc, CardTitle } from "../../styles/RegisterCards/Styles";
 const MemoryCard = ({ title, description, image }) => {
   return (
     <MCard className="">
-      <img src={image?.length > 0 ? image : memoryImg} alt="Memory" />
+      <img src={image?.length > 0 ? image : '/images/Memory.png'} alt="Memory" />
       <div className="text-center mt-4">
         <CardTitle>{title}</CardTitle>
         <CardDesc>{description}</CardDesc>
