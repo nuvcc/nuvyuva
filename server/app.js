04366f0715cc5ca19b534e54bccf99666f08fe6d
nimit2801 <mnimitsavant@outlook.com>
2023-02-23 16:19:28 +0530

    making ready for Azure Deployment

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 80;
 
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, '../build')));
@@ -12,6 +13,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
